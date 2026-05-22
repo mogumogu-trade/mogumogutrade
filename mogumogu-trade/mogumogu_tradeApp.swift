2d8b30b7637ca7b70dcaba7ccd1d031e20aef729
am2525nyan <chipdale411@gmail.com>
2026-05-22 22:49:36 +0900

    Initial Commit

---
+++ mogumogu-trade/mogumogu_tradeApp.swift
@@ -0,0 +1,17 @@
+//
+//  mogumogu_tradeApp.swift
+//  mogumogu-trade
+//
+//  Created by saki on 2026/05/22.
+//
+
+import SwiftUI
+
+@main
+struct mogumogu_tradeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
