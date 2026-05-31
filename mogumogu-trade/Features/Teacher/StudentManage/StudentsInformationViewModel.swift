b222e7b6f1c210109e14daae8d89aeb0889eab70
akipon <akinate.1011@gmail.com>
2026-05-31 12:33:10 +0900

    生徒を取得できるようになりました！

--- mogumogu-trade/Features/Teacher/StudentManage/StudentsInformationViewModel.swift
+++ mogumogu-trade/Features/Teacher/StudentManage/StudentsInformationViewModel.swift
@@ -1,7 +1,17 @@
 import Foundation
+import Dependencies
 
 @Observable
 @MainActor
 final class StudentsInformationViewModel{
     var students:[Student] = StudentMock.mockStudents
+    @ObservationIgnored @Dependency(\.studentClient) private var studentClient
+    
+    func getStudents() async {
+        do{
+            self.students = try await self.studentClient.getStudents("123456")
+        }catch{
+            print(error)
+        }
+    }
 }
